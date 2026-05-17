98ae5d9f7ba28e0031581e58a088ead7f4e34c66
John Gichuki <165212573+Saint2737@users.noreply.github.com>
2026-05-17 17:45:01 +0300

    Add files via upload

--- ChunkingController.java
+++ ChunkingController.java
@@ -1,21 +1,32 @@
+package com.cleverCRM.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cleverCRM.service.ChunkingService;
 
 @RestController
-@RequestMapping("/api/Chunking")
+@RequestMapping("/api/chunking")
 public class ChunkingController {
 
-	private final ChunkimgService chunkingService;
+	private final ChunkingService chunkingService;
 	
 	public ChunkingController(ChunkingService chunkingService) {
 		this.chunkingService = chunkingService;
 	}
 	
 	@PostMapping("/chunk")
-	public List<String> chunkText(@RequestBody String text, int maxChars, int overlap){
+	public List<String> chunkText(@RequestBody String text, @RequestParam int maxChars, @RequestParam int overlap){
 		return chunkingService.chunkText(text,maxChars,overlap);
 	}
 	
 	@PostMapping("/ingest")
-	public int ingestDocument(@RequestBody String sourceType, String sourceId, String content) {
+	public int ingestDocument(@RequestParam String sourceType, @RequestParam String sourceId, @RequestBody String content) {
 		return chunkingService.ingestDocument(sourceType,sourceId,content);
 	}
 	
